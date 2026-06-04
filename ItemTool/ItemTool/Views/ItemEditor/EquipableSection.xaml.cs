a6d650a05da4eb9e5cdd61507f1c1f900f6d07cf
Arnau Vilar <arnauvilar32@gmail.com>
2026-06-04 21:57:37 +0200

    Extract equipable and weapon item sections

---
+++ ItemTool/ItemTool/Views/ItemEditor/EquipableSection.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace ItemTool.App.Views.ItemEditor;
+
+public partial class EquipableSection : UserControl
+{
+    public EquipableSection()
+    {
+        InitializeComponent();
+    }
+}
---
+++ ItemTool/ItemTool/Views/ItemEditor/WeaponSection.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace ItemTool.App.Views.ItemEditor;
+
+public partial class WeaponSection : UserControl
+{
+    public WeaponSection()
+    {
+        InitializeComponent();
+    }
+}
