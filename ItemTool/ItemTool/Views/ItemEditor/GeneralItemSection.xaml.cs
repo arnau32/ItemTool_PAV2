711e45d713919cc8d90c749d212dbfb6e4f0553d
Arnau Vilar <arnauvilar32@gmail.com>
2026-06-04 21:52:00 +0200

    Extract general and slot dimension item sections

---
+++ ItemTool/ItemTool/Views/ItemEditor/GeneralItemSection.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace ItemTool.App.Views.ItemEditor;
+
+public partial class GeneralItemSection : UserControl
+{
+    public GeneralItemSection()
+    {
+        InitializeComponent();
+    }
+}
---
+++ ItemTool/ItemTool/Views/ItemEditor/SlotDimensionSection.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace ItemTool.App.Views.ItemEditor;
+
+public partial class SlotDimensionSection : UserControl
+{
+    public SlotDimensionSection()
+    {
+        InitializeComponent();
+    }
+}
