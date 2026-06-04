7959fbe4de8bcf1daaf477d298604692367f1170
Arnau Vilar <arnauvilar32@gmail.com>
2026-06-04 22:01:26 +0200

    Extract consumable and collectable item sections

---
+++ ItemTool/ItemTool/Views/ItemEditor/CollectableSection.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace ItemTool.App.Views.ItemEditor;
+
+public partial class CollectableSection : UserControl
+{
+    public CollectableSection()
+    {
+        InitializeComponent();
+    }
+}
---
+++ ItemTool/ItemTool/Views/ItemEditor/ConsumableSection.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace ItemTool.App.Views.ItemEditor;
+
+public partial class ConsumableSection : UserControl
+{
+    public ConsumableSection()
+    {
+        InitializeComponent();
+    }
+}
